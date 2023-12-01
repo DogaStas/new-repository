db24cb69526399911de250f89649fa9fe50cc7be
Stas Doga <doha.stanislav1123@vu.cdu.edu.ua>
2023-12-01 16:44:03 +0200

    Додано підрахунок суми чисел і виведення результату

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0},x + y");
+            Console.ReadKey();
+
+        }
+    }
+}
